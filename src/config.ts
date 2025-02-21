c7f51ef45bbad19307e3d673e4af977ededc285b
G4EVA-dev <tanzeglenn@gmail.com>
2025-02-21 23:36:12 +0100

    fix: remove config file

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-import telexConfig from "./config/telexconfig.json";
-import { TelexSetting, TelexConfig } from "./types";
-
-export const getConfig = (): TelexConfig => {
-  return telexConfig as TelexConfig;
-};
-
-export const DEFAULT_SETTINGS: TelexSetting[] = telexConfig.data
-  .settings as TelexSetting[];
